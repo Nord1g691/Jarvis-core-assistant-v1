84404aa15b5c2af0c11cb117892040f114e9ea5e
Nord1g691 <94491815+Nord1g691@users.noreply.github.com>
2026-08-30 14:55:09 +0200

    Restore V3 platform definitions

--- custom_components/jarvis/const.py
+++ custom_components/jarvis/const.py
@@ -1,6 +1,3 @@
 DOMAIN = "jarvis"
-PLATFORMS: list[str] = []
-PANEL_URL = "jarvis"
-PANEL_TITLE = "JARVIS"
-PANEL_ICON = "mdi:robot-outline"
-STATIC_URL = "/jarvis_static"
+PLATFORMS = ["sensor"]
+CONF_ENABLED = "enabled"
